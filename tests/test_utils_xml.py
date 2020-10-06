525eee8440aaf6c6da5201e55ea993a4bb438d30
Janis Beckedorf <janis@beckfish.de>
2020-10-06 10:18:27 +0200

    Setup

--- tests/test_utils_xml.py
+++ tests/test_utils_xml.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 
 from quantlaw.utils.xml import hello_world
 
+
 class UtilsXmlTestCase(TestCase):
     def test_hello_world(self):
-       self.assertEqual(hello_world(), 'x')
+        self.assertEqual(hello_world(), "x")
