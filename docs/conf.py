82bfa5a7d4f503794a199d15234dd8685ae07266
Janis Beckedorf <janis@beckfish.de>
2020-10-07 23:26:59 +0200

    fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,7 @@ __location__ = os.path.join(os.getcwd(), os.path.dirname(
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.join(__location__, '../src'))
+sys.path.append(os.path.abspath('../..'))
 sys.path.append(os.path.abspath('..'))
 
 # -- Run sphinx-apidoc ------------------------------------------------------
