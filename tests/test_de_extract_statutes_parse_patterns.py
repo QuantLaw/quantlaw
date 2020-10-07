55bdd5eb33d58990214236b7531a96fb64aeb378
Janis Beckedorf <janis@beckfish.de>
2020-10-08 01:49:04 +0200

    add reference parse (test)

---
+++ tests/test_de_extract_statutes_parse_patterns.py
@@ -0,0 +1,13 @@
+from unittest.case import TestCase
+
+from quantlaw.de_extract.statutes_patterns import reference_range_pattern
+
+
+class StatuteParsePatternsTextCase(TestCase):
+    def test_reference_areas_iVm_Art(self):
+        test_str = (
+            "nicht ohne Weiteres der Fall. Art. 2 Abs. 1 i.V.m. Art. 1 "
+            "Abs. 1 GG bietet nicht scho"
+        )
+        res = reference_range_pattern.search(test_str)
+        self.assertEqual(str(res[0]), "Art. 2 Abs. 1 i.V.m. Art. 1 Abs. 1")
