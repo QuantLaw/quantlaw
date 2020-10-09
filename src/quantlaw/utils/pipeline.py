2548edf3b8dfea214ff7ac3dd881c1b98c75a522
Janis Beckedorf <janis@beckfish.de>
2020-10-10 00:18:59 +0200

    add pipeline.py

---
+++ src/quantlaw/utils/pipeline.py
@@ -0,0 +1,44 @@
+import multiprocessing
+
+
+class PipelineStep:
+    max_number_of_processes = max(multiprocessing.cpu_count() - 2, 1)
+    chunksize = None
+
+    def __init__(self, processes=None, execute_args=[]):
+        self.processes = processes
+        self.execute_args = execute_args
+
+    def get_items(self) -> list:
+        raise Exception("This function must be implemented in the subclass")
+
+    def execute_item(self, item):
+        raise Exception("This function must be implemented in the subclass")
+
+    def execute_items(self, items):
+        ctx = multiprocessing.get_context()
+        processes = self.processes or self.__class__.max_number_of_processes
+
+        if processes > 1:
+            with ctx.Pool(processes) as p:
+                results = p.starmap(
+                    self.execute_item,
+                    [(i, *self.execute_args) for i in items],
+                    self.__class__.chunksize,
+                )
+        else:
+            results = []
+            for item in items:
+                results.append(self.execute_item(item, *self.execute_args))
+
+        return results
+
+    def execute_filtered_items(self, items, filters, *args, **kwargs):
+        filtered_items = []
+        for item in list(items):
+            for filter_str in filters:
+                if filter_str in item:
+                    filtered_items.append(item)
+                    break
+
+        return self.execute_items(filtered_items, *args, **kwargs)
---
+++ tests/test_utils_pipeline.py
@@ -0,0 +1,38 @@
+import unittest
+
+from quantlaw.utils.pipeline import PipelineStep
+
+
+class SampleStep(PipelineStep):
+    def get_items(self):
+        return range(50)
+
+    def execute_item(self, item, arg1):
+        return f"x{item}y{arg1}z"
+
+
+class PipelineTestCase(unittest.TestCase):
+    def test_abstrac_class(self):
+        step = PipelineStep()
+        with self.assertRaises(Exception):
+            step.get_items()
+        with self.assertRaises(Exception):
+            step.execute_item("asd")
+
+    def test_sample_step(self):
+        step = SampleStep(execute_args=["a"])
+        items = step.get_items()
+        result = step.execute_items(items)
+        self.assertEqual([f"x{i}yaz" for i in range(50)], result)
+
+    def test_sample_step_single_process(self):
+        step = SampleStep(processes=1, execute_args=["a"])
+        items = step.get_items()
+        result = step.execute_items(items)
+        self.assertEqual([f"x{i}yaz" for i in range(50)], result)
+
+    def test_filter(self):
+        step = SampleStep(execute_args=["a"])
+        items = ["aa", "bab", "cc", "caad"]
+        result = step.execute_filtered_items(items, ["aa", "cc"])
+        self.assertEqual(["xaayaz", "xccyaz", "xcaadyaz"], result)
