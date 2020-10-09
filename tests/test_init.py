43b28c5df096fb2db22e064e9b937a36fa5902c7
Janis Beckedorf <janis@beckfish.de>
2020-10-09 15:01:38 +0200

    fix tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -2,8 +2,10 @@ import unittest
 from pkg_resources import DistributionNotFound
 from unittest.mock import patch
 
+
 def version_test_side_effect(args):
     raise DistributionNotFound()
+
 
 class InitTestCase(unittest.TestCase):
     @patch("pkg_resources.get_distribution")
