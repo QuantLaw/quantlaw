9fcc3173f3f8cce56509bfda4570af998a7aac6a
Janis Beckedorf <janis@beckfish.de>
2020-10-09 15:57:19 +0200

    fix tests

--- src/quantlaw/__init__.py
+++ src/quantlaw/__init__.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
-from pkg_resources import DistributionNotFound, get_distribution
 
-try:
-    # Change here if project is renamed and does not equal the package name
-    dist_name = __name__
-    __version__ = get_distribution(dist_name).version
-except DistributionNotFound:
-    __version__ = "unknown"
-finally:
-    del get_distribution, DistributionNotFound
+dist_name = __name__
+
+
+def get_quantlaw_package_version():
+    from pkg_resources import DistributionNotFound, get_distribution
+
+    try:
+        # Change here if project is renamed and does not equal the package name
+        version = get_distribution(dist_name).version
+    except DistributionNotFound:
+        version = "unknown"
+
+        return version
+
+
+__version__ = get_quantlaw_package_version()
