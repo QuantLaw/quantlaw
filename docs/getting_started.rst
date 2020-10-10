4b65c0ae32db214139986fc1cad46881c263b909
Janis Beckedorf <janis@beckfish.de>
2020-10-10 12:20:37 +0200

    add minimal example

---
+++ docs/getting_started.rst
@@ -0,0 +1,26 @@
+.. _getting_started:
+
+===============
+Getting Started
+===============
+
+------------
+Installation
+------------
+
+You need to have python installed. Python 3.7 is recommended but other versions
+will likely work as well.
+
+Install with
+
+::
+
+    pip install quantlaw
+
+
+---------------
+Minimal example
+---------------
+
+
+.. literalinclude:: ../example/example.py
---
+++ example/example.py
@@ -0,0 +1,39 @@
+import json
+
+from quantlaw.de_extract.statutes_areas import StatutesExtractor
+from quantlaw.de_extract.statutes_parse import StatutesParser
+from quantlaw.de_extract.stemming import stem_law_name
+
+if __name__ == "__main__":
+    # Load test that will be analyzed
+    with open("paragraph_120_gvg.txt", encoding="utf8") as f:
+        text = f.read()
+
+    # Load a list of law names
+    with open("law_names.json", encoding="utf8") as f:
+        law_names_raw = json.load(f)
+
+    # Stem law names and also allow discovery by abbreviations
+    law_names = {}
+
+    for law_name, law_abbreviation in law_names_raw.items():
+        law_names[stem_law_name(law_name)] = law_abbreviation
+        law_names[stem_law_name(law_abbreviation)] = law_abbreviation
+
+    # Instantiate extractor
+    extractor = StatutesExtractor(laws_lookup=law_names)
+
+    # Extract areas
+    matches = list(extractor.find_all(text))
+
+    # Instantiate parser
+    parser = StatutesParser(law_names)
+
+    # Parse each reference
+    for match in matches:
+        if match.has_main_area():
+            main_area_data = parser.parse_main(match.main_text())
+            law_name_area_data = parser.parse_law(
+                match.law_text(), match.law_match_type, current_lawid="GVG"
+            )
+            print(main_area_data, "-", law_name_area_data)
