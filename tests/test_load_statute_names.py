f336cff187aa76a8cd3fc2e20e8f903859f45a38
Janis Beckedorf <janis@beckfish.de>
2020-10-25 00:09:42 +0200

    Add load law names (add tests)

---
+++ tests/test_load_statute_names.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+from src.quantlaw.de_extract.load_statute_names import load_law_names
+
+
+class LoadStatueNamesTestCase(TestCase):
+    def test_load_statue_names(self):
+        load_law_names("2020-10-20", "test_law_names.json")
